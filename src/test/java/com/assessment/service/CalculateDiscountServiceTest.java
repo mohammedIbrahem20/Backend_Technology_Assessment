f1a502358e8730815980708e52d64521595bec28
mohammed elhusary <Mohammed.elhusary@gmail.com>
2024-02-06 16:34:04 +0200

    edit test cases

--- src/test/java/com/assessment/service/CalculateDiscountServiceTest.java
+++ src/test/java/com/assessment/service/CalculateDiscountServiceTest.java
@@ -1,11 +1,8 @@
 package com.assessment.service;
 
-import static org.mockito.Mockito.when;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,8 +12,6 @@ import com.assessment.model.UserType;
 @SpringBootTest
 class CalculateDiscountServiceTest {
 
-//	@Mock
-//	private Bill bill;
 	Bill bill;
 
 	@Autowired
@@ -28,81 +23,59 @@ class CalculateDiscountServiceTest {
 	}
 
 	@Test
-    public void testCalculateNetPayableAmountForEmployeeDiscount() {
-		
+	public void testCalculateNetPayableAmountForEmployeeDiscount() {
+
 		bill.setGrocery(false);
 		bill.setTotalAmount(100.0);
 		bill.setUserType(UserType.EMPLOYEE);
-		
-//        when(bill.getTotalAmount()).thenReturn(100.0);
-//        when(bill.isGrocery()).thenReturn(false);
-//        when(bill.getUserType()).thenReturn(UserType.EMPLOYEE);
 
-        double netPayableAmount = discountService.calculateNetPayableAmount(bill);
+		double netPayableAmount = discountService.calculateNetPayableAmount(bill);
 
-        Assertions.assertEquals(65.0, netPayableAmount);
-    }
+		Assertions.assertEquals(65.0, netPayableAmount);
+	}
 
 	@Test
-    public void testCalculateNetPayableAmountForAffiliateDiscount() {
-//        when(bill.getTotalAmount()).thenReturn(100.0);
-//        when(bill.isGrocery()).thenReturn(false);
-//        when(bill.getUserType()).thenReturn(UserType.AFFILIATE);
-
-//		Bill bill = new Bill();
+	public void testCalculateNetPayableAmountForAffiliateDiscount() {
 		bill.setGrocery(false);
 		bill.setTotalAmount(100.0);
 		bill.setUserType(UserType.AFFILIATE);
-		
-        double netPayableAmount = discountService.calculateNetPayableAmount(bill);
 
-        Assertions.assertEquals(85.0, netPayableAmount);
-    }
+		double netPayableAmount = discountService.calculateNetPayableAmount(bill);
+
+		Assertions.assertEquals(85.0, netPayableAmount);
+	}
 
 	@Test
-    public void testCalculateNetPayableAmountForLongTermCustomerDiscount() {
-//        when(bill.getTotalAmount()).thenReturn(100.0);
-//        when(bill.isGrocery()).thenReturn(false);
-//        when(bill.getUserType()).thenReturn(UserType.LONG_TERM_CUSTOMER);
-		
-//		Bill bill = new Bill();
+	public void testCalculateNetPayableAmountForLongTermCustomerDiscount() {
 		bill.setGrocery(false);
 		bill.setTotalAmount(100.0);
 		bill.setUserType(UserType.LONG_TERM_CUSTOMER);
 
-        double netPayableAmount = discountService.calculateNetPayableAmount(bill);
+		double netPayableAmount = discountService.calculateNetPayableAmount(bill);
 
-        Assertions.assertEquals(90.0, netPayableAmount);
-    }
+		Assertions.assertEquals(90.0, netPayableAmount);
+	}
 
 	@Test
-    public void testCalculateNetPayableAmountHundredDollarDiscount() {
-//        when(bill.getTotalAmount()).thenReturn(990.0);
-//        when(bill.isGrocery()).thenReturn(true);
-//        when(bill.getUserType()).thenReturn(null);
-		
-//		Bill bill = new Bill();
+	public void testCalculateNetPayableAmountHundredDollarDiscount() {
 		bill.setGrocery(true);
 		bill.setTotalAmount(990.0);
 		bill.setUserType(null);
 
-        double netPayableAmount = discountService.calculateNetPayableAmount(bill);
+		double netPayableAmount = discountService.calculateNetPayableAmount(bill);
 
-        Assertions.assertEquals(945.0, netPayableAmount);
-    }
+		Assertions.assertEquals(945.0, netPayableAmount);
+	}
 
 	@Test
-    public void testCalculateNetPayableAmountnODiscount() {
-//        when(bill.getTotalAmount()).thenReturn(50.0);
-//        when(bill.isGrocery()).thenReturn(true);
-//		Bill bill = new Bill();
+	public void testCalculateNetPayableAmountnODiscount() {
 		bill.setGrocery(true);
 		bill.setTotalAmount(50.0);
 		bill.setUserType(null);
 
-        double netPayableAmount = discountService.calculateNetPayableAmount(bill);
+		double netPayableAmount = discountService.calculateNetPayableAmount(bill);
 
-        Assertions.assertEquals(50.0, netPayableAmount);
-    }
+		Assertions.assertEquals(50.0, netPayableAmount);
+	}
 
 }
